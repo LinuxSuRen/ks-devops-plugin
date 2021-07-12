a8398b03e50ed84b086188ca977128d5b64cb138
rick <linuxsuren@users.noreply.github.com>
2021-07-12 20:48:26 +0800

    Import kubesphere devops plugin project

---
+++ pkg/apiserver/authentication/request/anonymous/anonymous.go
@@ -0,0 +1,27 @@
+package anonymous
+
+import (
+	"k8s.io/apiserver/pkg/authentication/authenticator"
+	"k8s.io/apiserver/pkg/authentication/user"
+	"devops.kubesphere.io/plugin/pkg/apiserver/query"
+	"net/http"
+)
+
+// Authenticator implements an anonymous auth
+type Authenticator struct{}
+
+func NewAuthenticator() authenticator.Request {
+	return &Authenticator{}
+}
+
+func (a *Authenticator) AuthenticateRequest(req *http.Request) (*authenticator.Response, bool, error) {
+	if auth := query.GetAuthorization(req.Header); auth == "" {
+		return &authenticator.Response{
+			User: &user.DefaultInfo{
+				Name:   "anonymous",
+				Groups: []string{user.AllAuthenticated},
+			},
+		}, true, nil
+	}
+	return nil, false, nil
+}
